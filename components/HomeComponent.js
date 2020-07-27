3befb3db490cdaf7e87649e06019498c432963da
Peyton <pcopcoeco@gmail.com>
2020-07-27 16:51:48 -0500

    Some Home Screen

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+import { ARTICLES } from '../shared/articles'
+
+function RenderItem({item}) {
+    if (item) {
+        return ( 
+            <Card
+                featuredTitle={item.title}
+                image={require('./images/italy.jpeg')}>
+                <Text
+                    style={{margin: 10}}>
+                    {item.author}
+                </Text>
+            </Card>
+        );
+    }
+    return <View />;
+}
 
 class Home extends Component {
     
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            articles: ARTICLES
+        };
+    }
+
     static navigationOptions = {
         title: 'Home'
     }
 
     render() {
         return (
-            <View>
-                <Text>Home Component</Text>
-            </View>
+            <ScrollView>
+                <Card
+                    title='Our Mission'>
+                    <Text 
+                        style={{margin:10}}>
+                        Media loves to resprent the youngsters. If you are an extrodinairy 50 year old look for inspiration, you've come to the right place!
+                    </Text>
+                </Card>
+                <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} >
+                    <Text style={{fontSize: 16, marginTop: 10, fontWeight: "bold"}}>Featured Article</Text>
+                </View>
+                <RenderItem 
+                    item={this.state.articles.filter(article => article.featured)[0]} />
+            </ScrollView>
         );
     }
 }
